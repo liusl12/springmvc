13b08865ceda9976511c9a366980be57098510f1
Liu <liusl12@lenovo.com>
2017-11-14 15:17:41 +0800

    Test

--- src/main/java/com/liusl/controller/HomeController.java
+++ src/main/java/com/liusl/controller/HomeController.java
@@ -4,22 +4,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * created by l1 on 2017/10/30.
  */
 @Controller
-@RequestMapping("/home")
 public class HomeController {
     //添加一个日志器
     private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     //映射一个action
-    @RequestMapping("/index")
-    public  String index(){
+    @RequestMapping(value="/home",method = RequestMethod.GET)
+    public  String home(){
         //输出日志文件
         logger.info("the first jsp pages");
         //返回一个index.jsp这个视图
-        return "index";
+        return "home";
     }
 }
